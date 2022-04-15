6654def57392b792ca714153cc36b9acee7d2550
Ravi Ashara <ravi.ashara@atharvasystem.com>
2022-04-15 19:12:21 +0530

    Add fields in home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,53 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput,Button,Alert} from 'react-native';
+import styles from './styles/DefaultStyle';
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fname:'', lname:'', mno:'', address:'' };
+  }
+  buttonClick = () => {
+    if(!this.state.fname.trim()){
+      Alert.alert('Alert','Enter First Name');
+      return;
+    }
+    if(!this.state.lname.trim()){
+      Alert.alert('Alert','Enter Last Name');
+      return;
+    }
+    if(!this.state.mno.trim()){
+      Alert.alert('Alert','Enter Mobile Number');
+      return;
+    }
+    if(this.state.mno.trim().length < 6){
+      Alert.alert('Alert','Enter Mobile Number at least 6 digits');
+      return;
+    }
+    if(!this.state.address.trim()){
+      Alert.alert('Alert','Enter Address');
+      return;
+    }
+  };
   render() {
     return (
       <View>
-        <Text style={{ marginBottom: 10 }}>Hello World</Text>
+        <TextInput placeholder='First Name' style={styles.input} onChangeText={(text) => this.setState({ fname: text })}/>
+        <TextInput placeholder='Last Name' style={styles.input} onChangeText={(text) => this.setState({ lname: text })}/>
+        <TextInput
+          placeholder='Mobile Number'
+          style={styles.input}
+          keyboardType='phone-pad'
+          onChangeText={(text) => this.setState({ mno: text })}
+        />
+        <TextInput
+          placeholder='Address'
+          style={styles.textarea}
+          multiline={true}
+          onChangeText={(text) => this.setState({ address: text })}
+        />
+        <View style={styles.centerAlign}>
+          <Button title='Submit' onPress={() => this.buttonClick()} />
+        </View>
       </View>
     );
   }
